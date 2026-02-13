c3400b44dd94c8afada937b56a622c6489dc0a50
Carlos Villarreal <150401546+Crasty49@users.noreply.github.com>
2026-02-12 21:41:43 -0600

    Cantidad de menú

--- src/data/menu.js
+++ src/data/menu.js
@@ -6,40 +6,53 @@ export const sauces = [
 ];
 
 export const menu = [
+
   {
     id: 1,
     name: "Boneless (12 pz)",
     price: 140,
     hasSauce: true
   },
+
   {
     id: 2,
     name: "Media orden Boneless (6 pz)",
     price: 80,
     hasSauce: true
   },
+
+  // 🔥 productos con selector de cantidad
+
   {
     id: 3,
-    name: "Papas gajo",
+    name: "Orden de papas gajo",
     price: 60,
-    hasSauce: false
+    hasSauce: false,
+    hasQuantity: true
   },
+
   {
     id: 4,
-    name: "Papas a la francesa",
+    name: "Orden de papas a la francesa",
     price: 55,
-    hasSauce: false
+    hasSauce: false,
+    hasQuantity: true
   },
+
   {
     id: 5,
     name: "Dedos de queso (12 pz)",
     price: 130,
-    hasSauce: false
+    hasSauce: false,
+    hasQuantity: true
   },
+
   {
     id: 6,
     name: "Media orden dedos de queso (6 pz)",
     price: 75,
-    hasSauce: false
+    hasSauce: false,
+    hasQuantity: true
   }
+
 ];
